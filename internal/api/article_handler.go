9e8e24189ef22564764887ef79fee9e2b43d5a11
NurulloMahmud <nurullomahmud@gmail.com>
2025-11-03 11:08:48 -0600

    update article v1

--- internal/api/article_handler.go
+++ internal/api/article_handler.go
@@ -58,3 +58,58 @@ func (ah *ArticleHandler) HandleCreateArticle(w http.ResponseWriter, r *http.Req
 
 	utils.WriteJSON(w, http.StatusCreated, utils.Envelope{"article": createdArticle})
 }
+
+func (ah *ArticleHandler) HandleUpdateArticle(w http.ResponseWriter, r *http.Request) {
+	articleID, err := utils.ReadIDParam(r)
+	if err != nil {
+		ah.logger.Printf("error converting id: %v", err)
+		utils.WriteJSON(w, http.StatusBadRequest, utils.Envelope{"error": "invalid id parameter"})
+		return
+	}
+
+	existingArticle, err := ah.articleStore.GetArticleByID(articleID)
+	if err != nil {
+		ah.logger.Printf("internal server error: %v", err)
+		utils.WriteJSON(w, http.StatusInternalServerError, utils.Envelope{"error": "internal server error"})
+		return
+	}
+
+	if existingArticle == nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	var updatedArticle struct {
+		Title      *string           `json:"title"`
+		Image      *string           `json:"image"`
+		AuthorID   *int              `json:"author_id"`
+		Paragraphs []store.Paragraph `json:"paragraphs"`
+	}
+
+	err = json.NewDecoder(r.Body).Decode(&updatedArticle)
+	if err != nil {
+		ah.logger.Printf("error decoding payload: %v", err)
+		utils.WriteJSON(w, http.StatusBadRequest, utils.Envelope{"error": "invalid payload format"})
+		return
+	}
+
+	if updatedArticle.Title != nil {
+		existingArticle.Title = *updatedArticle.Title
+	}
+	if updatedArticle.Image != nil {
+		existingArticle.Image = *updatedArticle.Image
+	}
+	if updatedArticle.Paragraphs != nil {
+		existingArticle.Paragraphs = updatedArticle.Paragraphs
+	}
+
+	// later we implement some validations here
+
+	err = ah.articleStore.UpdateArticle(existingArticle)
+	if err != nil {
+		ah.logger.Printf("error updating article: %v", err)
+		utils.WriteJSON(w, http.StatusInternalServerError, utils.Envelope{"error": "internal server error"})
+	}
+
+	utils.WriteJSON(w, http.StatusOK, utils.Envelope{"article": existingArticle})
+}
