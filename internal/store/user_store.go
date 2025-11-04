8bf3e1c03815a98e7d41331b3aa0dc41b7b05bd2
NurulloMahmud <nurullomahmud@gmail.com>
2025-11-04 17:26:06 -0600

    user store v1

--- internal/store/user_store.go
+++ internal/store/user_store.go
@@ -2,6 +2,7 @@ package store
 
 import (
 	"database/sql"
+	"fmt"
 )
 
 type User struct {
@@ -21,35 +22,81 @@ func NewPostgresUserStore(db *sql.DB) *PostgresUserStore {
 type UserStore interface {
 	CreateUser(*User) (*User, error)
 	GetUserByID(id int64) (*User, error)
+	UpdateUser(*User) (*User, error)
+	DeleteUser(id int64) error
 }
 
 func (pg *PostgresUserStore) CreateUser(user *User) (*User, error) {
-	tx, err := pg.db.Begin()
-	if err != nil {
-		return nil, err
-	}
-	defer tx.Rollback()
-
 	query := `
-	INSERT INTO user (email, hashed_password)
+	INSERT INTO users(email, hashed_password)
 	VALUES ($1, $2)
 	RETURNING id
 	`
-
-	// handle password hashing later
-	err = tx.QueryRow(query, user.Email, user.HashedPassword).Scan(&user.ID)
-	if err != nil {
-		return nil, err
-	}
-	err = tx.Commit()
+	err := pg.db.QueryRow(query, user.Email, user.HashedPassword).Scan(&user.ID)
 	if err != nil {
 		return nil, err
 	}
 
+	// handle validations and edge cases later
 	return user, nil
 }
 
 func (pg *PostgresUserStore) GetUserByID(id int64) (*User, error) {
 	user := &User{}
+	query := `
+	SELECT id, email, hashed_password
+	FROM users
+	WHERE id = $1
+	`
+	err := pg.db.QueryRow(query, id).Scan(&user.ID, &user.Email, &user.HashedPassword)
+	if err == sql.ErrNoRows {
+		return nil, nil // so we can catch not found easly in api layer
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
 	return user, nil
+}
+
+func (pg *PostgresUserStore) UpdateUser(user *User) (*User, error) {
+	updatedUser := &User{}
+	query := `
+	UPDATE users
+	SET email = $1, hashed_password = $2
+	WHERE id = $3
+	RETURNING id, email, hashed_password
+	`
+	err := pg.db.QueryRow(query, user.Email, user.HashedPassword).Scan(&updatedUser.ID, &updatedUser.Email, &updatedUser.HashedPassword)
+	if err == sql.ErrNoRows {
+		return nil, nil
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return updatedUser, nil
+}
+
+func (pg *PostgresUserStore) DeleteUser(id int64) error {
+	query := `
+	DELETE FROM users WHERE id = $1
+	`
+	result, err := pg.db.Exec(query, id)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return fmt.Errorf("no user found with id %d: %w", id, sql.ErrNoRows)
+	}
+
+	return nil
 }
